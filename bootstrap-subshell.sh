86b9056aacf3738ce6c2a8aa789d6d7aa2ea0f27
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-10-24 13:04:58 +0300

    Allow subshell location customization

--- bootstrap-subshell.sh
+++ bootstrap-subshell.sh
@@ -2,30 +2,35 @@
 
 set -x
 set -eu
-orig_home=$HOME
-export HOME=$HOME/.epeli
+orig_home="$HOME"
 
-mkdir $HOME
-cd $HOME
+new_home="$HOME/epeli"
+read -p "Subshell HOME[$HOME/epeli]> " custom_home
+[ "$custom_home" != "" ] && new_home="$custom_home"
+
+
+mkdir $new_home
+cd $new_home
 echo ". $orig_home/.bashrc" > ".bashrc"
-echo ". $HOME/.bashrc" > ".bash_profile"
+echo ". $new_home/.bashrc" > ".bash_profile"
 
-cat > $HOME/login<<EOF
+cat > $new_home/login<<EOF
 #!/bin/sh
-export HOME=$HOME
+export HOME=$new_home
 echo
 echo "Starting new bash shell Vim superpowers!"
 echo
 exec bash --login
 EOF
-chmod +x $HOME/login
+chmod +x $new_home/login
 
-wget -q --no-check-certificate -O - https://github.com/epeli/vimconfig/raw/master/bootstrap.sh  | sh
-cd $HOME/.vim
+export HOME="$new_home"
+wget -qO - https://github.com/epeli/vimconfig/raw/master/bootstrap.sh  | sh
+cd $new_home/.vim
 make friends
 
 set +x
 echo
-echo "Subshell installed. Start it with ~/.epeli/login"
+echo "Subshell installed. Start it with $new_home/login"
 echo
 
